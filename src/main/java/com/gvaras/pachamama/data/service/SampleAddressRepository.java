51410441dfc1366a496d989fb600776adda85894
start.vaadin.com <no-reply@vaadin.com>
2023-06-24 21:35:46 +0000

    Generated project

---
+++ src/main/java/com/gvaras/pachamama/data/service/SampleAddressRepository.java
@@ -0,0 +1,12 @@
+package com.gvaras.pachamama.data.service;
+
+import com.gvaras.pachamama.data.entity.SampleAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SampleAddressRepository
+        extends
+            JpaRepository<SampleAddress, Long>,
+            JpaSpecificationExecutor<SampleAddress> {
+
+}
---
+++ src/main/java/com/gvaras/pachamama/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package com.gvaras.pachamama.data.service;
+
+import com.gvaras.pachamama.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
